782c942ffceb627604b5d59fb555510283253c9a
Lee heonjun <143599675+jun980724@users.noreply.github.com>
2023-10-04 19:59:18 +0900

    Update main.py
    오타 수정

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def twoSum(nums: List[int], target: int) -> List[int]:
 
     return []
 
-#text case
+#test case
 print(twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
 print(twoSum([3, 2, 4], 6))       # Output: [1, 2]
 print(twoSum([3, 3], 6))          # Output: [0, 1]
